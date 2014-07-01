7f9a8f5e86de041e5dd8d9bec87bacf852dc0ffd
rishabh16 <rishabh.16.singh@gmail.com>
2014-07-01 06:25:39 -0400

    Create Digit.java

---
+++ Digit.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class Digit
+{
+	public static void main(String args[]) throws IOException
+	{
+		InputStreamReader isr= new InputStreamReader(System.in);
+		BufferedReader br= new BufferedReader(isr);
+		System.out.println("Enter any no.-");
+		int n=0;
+		
+			n = Integer.parseInt(br.readLine());
+		
+		int digit, i;
+		for(i=n;i>0;i=i/10)
+		{	
+			digit=i%10;
+			System.out.println(digit);
+		}
+	}
+}
